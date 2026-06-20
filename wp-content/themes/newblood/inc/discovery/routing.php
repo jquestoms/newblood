a76b41477971bc78f061dc7e56b21e8d3b762278
Jeremy Oms <joms@newblood.com>
2026-06-19 17:29:20 -0700

    feat(discovery-report): /discovery/{slug}/report route (rewrite v2)

--- wp-content/themes/newblood/inc/discovery/routing.php
+++ wp-content/themes/newblood/inc/discovery/routing.php
@@ -1,15 +1,17 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'NB_DISCOVERY_REWRITE_VERSION', '1' );
+define( 'NB_DISCOVERY_REWRITE_VERSION', '2' );
 
 function nb_discovery_register_query_var( $vars ) {
     $vars[] = 'nb_discovery';
+    $vars[] = 'nb_discovery_report';
     return $vars;
 }
 add_filter( 'query_vars', 'nb_discovery_register_query_var' );
 
 function nb_discovery_register_rewrite() {
+    add_rewrite_rule( '^discovery/([^/]+)/report/?$', 'index.php?nb_discovery=$matches[1]&nb_discovery_report=1', 'top' );
     add_rewrite_rule( '^discovery/([^/]+)/?$', 'index.php?nb_discovery=$matches[1]', 'top' );
 }
 add_action( 'init', 'nb_discovery_register_rewrite' );
